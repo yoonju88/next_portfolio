f7cd7c0c79acb31b2f660dbb497ba11c17c61e99
yoonju88 <yoonju.t@gmail.com>
2024-11-25 11:56:30 +0100

    changed logo color

--- components/nav/Header.js
+++ components/nav/Header.js
@@ -13,7 +13,7 @@ export default function header() {
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link href='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" >
-                    <span className="ml-3 text-xl text-cyan-600" >Yoonju.T</span>
+                    <span className="ml-3 text-xl text-indigo-500" >Yoonju.T</span>
                 </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                     {links.map((link) => {
