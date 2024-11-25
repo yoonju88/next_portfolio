132151f17d271f7492123ac1b2b3c0a58c16be59
yoonju88 <yoonju.t@gmail.com>
2024-11-25 11:56:09 +0100

    generate project cards

--- components/works/ProjectCard.js
+++ components/works/ProjectCard.js
@@ -1,11 +1,32 @@
 import React from 'react'
-import { worksData } from '@/utils/worksData'
+import Image from 'next/image'
+import Link from 'next/link'
 
-export default function ProjectCard() {
-
+export default function ProjectCard({ works }) {
     return (
-        <>
-            <h1>ProjectCard</h1>
-        </>
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+
+            {
+                works.map((item) => {
+                    return (
+                        <div className="p-4 md:w-1/3 sm:mb-0 mb-6 mt-3">
+                            <div className="rounded-lg h-60 overflow-hidden">
+                                <Image alt={item.title} className="object-cover object-center h-full w-full" src={item.image} />
+                            </div>
+                            <h2 className="text-xl font-medium title-font text-gray-900 mt-6">{item.title}</h2>
+                            <p className="text-base leading-relaxed mt-2">
+                                {item.description}
+                            </p>
+                            <Link className="text-indigo-500 inline-flex items-center mt-3" href={`/works/${item.id}`} >
+                                Learn More
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                </svg>
+                            </Link>
+                        </div>
+                    )
+                })}
+
+        </div >
     )
 }
--- components/works/ProjectTitle.js
+++ components/works/ProjectTitle.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 export default function ProjectTypeTitle({ title, description }) {
     return (
-        <div className="flex flex-wrap items-center w-full mb-20">
-            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">{title}</h1>
-                <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+        <div className="flex flex-col items-center gap-10  w-full mb-20">
+            <div className="mb-6 lg:mb-0">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-indigo-500 capitalize">{title}</h1>
             </div>
-            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">{description}</p>
+            <p className="lg:w-2/3 w-full leading-relaxed text-gray-500">{description}</p>
         </div>
     )
 }
