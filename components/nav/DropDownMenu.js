0fbae57e14f545cabae5242dd248876d81665516
yoonju88 <yoonju.t@gmail.com>
2024-11-26 13:24:32 +0100

    add router for works

---
+++ components/nav/DropDownMenu.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from 'next/link'
+
+export default function DropDownMenu({ links, className }) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center gap-1">
+                <Link
+                    href='/works'
+                    className={className}
+                >
+                    works
+                </Link>
+                <span className="sr-only">Works</span>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center">
+                {links.map((link) => {
+                    return (
+                        <DropdownMenuItem key={link.linkName}>
+                            <Link href={link.href} >
+                                {link.linkName}
+                            </Link>
+                        </DropdownMenuItem>
+                    )
+                })}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
--- components/nav/Header.js
+++ components/nav/Header.js
@@ -2,14 +2,16 @@ import React from 'react'
 import DarkMode from './DarkMode'
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
+import DropDownMenu from './DropDownMenu'
 
-const links = [
-    { href: '/', linkName: 'home' },
-    { href: '/works', linkName: 'works' },
-    { href: '/about-me', linkName: 'about me' },
-    { href: '/contact', linkName: 'contact' },
+const worksLinks = [
+    { href: '/works/web', linkName: 'developement web' },
+    { href: '/works/design', linkName: 'graphic design' },
 ]
 export default function header() {
+
+    const linkClassName = 'mr-5 capitalize text-foreground  hover:font-medium transition-colors duration-200'
+
     return (
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -17,17 +19,32 @@ export default function header() {
                     <span className="ml-3 text-xl text-white font-semibold" >YT</span>
                 </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    {links.map((link) => {
-                        return (
-                            <Link
-                                href={link.href}
-                                className="mr-5 capitalize text-foreground  hover:font-medium transition-colors duration-200"
-                                key={link.href}
-                            >
-                                {link.linkName}
-                            </Link>
-                        )
-                    })}
+
+                    <Link
+                        href='/'
+                        className={linkClassName}
+                    >
+                        home
+                    </Link>
+                    <DropDownMenu
+                        className={linkClassName}
+                        links={worksLinks}
+                    />
+                    <Link
+                        href='/about-me'
+                        className={linkClassName}
+                    >
+                        about me
+                    </Link>
+                    <Link
+                        href='/contact'
+                        className={linkClassName}
+                    >
+                        contact
+                    </Link>
+
+
+
                 </nav>
                 <DarkMode />
             </div>
