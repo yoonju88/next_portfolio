bd0b58d54b65101b9aaf6bc0199254cef8bb1d4f
yoonju88 <yoonju.t@gmail.com>
2024-11-26 17:07:28 +0100

    modified href

--- components/works/ProjectList.js
+++ components/works/ProjectList.js
@@ -11,7 +11,7 @@ export default function ProjectList({ works }) {
                     return (
                         <div key={item.id} className="p-4 md:w-1/3 sm:mb-0 mb-6 mt-3">
                             <div className="rounded-lg h-60 overflow-hidden">
-                                <Link href={`/works/${item.id}`} >
+                                <Link href={`/works/web/${item.id}`} >
                                     <Image alt={item.title} className="object-cover object-center h-full w-full transform transition duration-300 hover:scale-105" src={item.image} />
                                 </Link>
                             </div>
@@ -19,7 +19,7 @@ export default function ProjectList({ works }) {
                             <p className="text-base leading-relaxed mt-2 text-foreground">
                                 {item.description}
                             </p>
-                            <Link className="inline-flex items-center mt-3 text-primary transform transition-all duration-200 hover:font-semibold" href={`/works/${item.id}`} >
+                            <Link className="inline-flex items-center mt-3 text-primary transform transition-all duration-200 hover:font-semibold" href={`/works/web/${item.id}`} >
                                 Learn More
                                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
