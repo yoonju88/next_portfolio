ae7fc4b40c1acafd32721e7ca92a65a972a94344
yoonju88 <yoonju.t@gmail.com>
2024-11-26 17:07:10 +0100

    Change style

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "border bg-white drop-shadow-md rounded hover:drop-shadow-none hover:shadow-inner",
       },
       size: {
         default: "h-10 px-4 py-2",
