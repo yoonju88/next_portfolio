76a995a123c3abb871b8cd8df0a880f683a52f39
yoonju88 <yoonju.t@gmail.com>
2024-11-27 14:29:57 +0100

    modi

--- components/works/Tags.js
+++ components/works/Tags.js
@@ -6,10 +6,10 @@ export default function Tags({ name, tags }) {
     return (
         <>
             <span class="text-secondary-foreground text-sm capitalize">{name}</span>
-            <ul className="flex gap-1 items-center ">
+            <ul className="flex gap-0 items-center ">
                 {tags.map((tag) => {
                     return (
-                        <li key={tag} className={`text-xs text-foreground py-1 px-1.5 font-normal rounded ${animationSet} hover:font-medium hover:bg-accent`} >{tag}</li>
+                        <li key={tag} className={`text-xs text-foreground py-1 px-1.5 font-normal rounded ${animationSet} hover:text-white hover:bg-primary/90`} >{tag}</li>
                     )
                 })}
             </ul>
