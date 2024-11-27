a72bdabeacd42a69b57104fa712ba8084341374f
yoonju88 <yoonju.t@gmail.com>
2024-11-27 14:28:33 +0100

    Create the button component but be determine the style

--- components/Buttons.js
+++ components/Buttons.js
@@ -2,16 +2,29 @@ import { Button } from "@/components/ui/button"
 import Link from "next/link"
 
 
-export const ButtonLink = ({ href, name, children }) => {
+export const ButtonLink = ({ href, name, size, variant, children }) => {
 
     return (
-        <div className="border-t border-gray-200 flex justify-center py-4">
-            <Button variant="link" size="sm" className="mt-1">
-                <Link href={href} target='_blank' className="flex flex-inline items-center gap-2">
-                    {name}
-                    {children}
-                </Link>
-            </Button>
-        </div >
+        <Button variant={variant} size={size} className="mt-1" >
+            <Link href={href} target='_blank' className="flex flex-inline items-center gap-2 text-background">
+                {name}
+                {children}
+            </Link>
+        </Button >
     )
 }
+
+export const ButtonNavigate = ({ href, name, size, variant, children }) => {
+    return (
+        <div className="flex justify-center">
+            <Button variant={variant} size={size}>
+                <Link href='/works/web' className="capitalize flex items-center">
+                    {name}
+                </Link>
+                {children}
+            </Button>
+        </div>
+    )
+
+}
+
