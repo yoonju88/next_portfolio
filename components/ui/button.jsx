278fe5cbb9fd524f7b2cf0392b4829a0b6b26efe
yoonju88 <yoonju.t@gmail.com>
2024-11-27 14:29:30 +0100

    changed theme on variant

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -16,14 +16,14 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-secondary hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "border bg-white drop-shadow-md rounded hover:drop-shadow-none hover:shadow-inner",
+        ghost: "hover:bg-primary/60 hover:text-accent-foreground",
+        link: "text-background border bg-foreground drop-shadow-md rounded hover:bg-primary/90 hover:text-white",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-8 w-8",
       },
     },
     defaultVariants: {
