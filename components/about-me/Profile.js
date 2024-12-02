88f30138ccaa38d6229a066a4eabd7556e5e4373
yoonju88 <yoonju.t@gmail.com>
2024-12-02 15:04:30 +0100

    update profile section

--- components/about-me/Profile.js
+++ components/about-me/Profile.js
@@ -2,14 +2,13 @@ import React from 'react'
 import ProfileYJ from "@/public/yoonju-profil.jpg"
 import Image from 'next/image'
 import { ButtonLink } from '@/components/Buttons'
-import ToolTip from '../ToolTip'
 
 
 const profilInfo = [
     {
         src: ProfileYJ,
         name: "TERRENOIRE Yoonjiu",
-        description: "Ayant développé un intérêt pour le codage, je suis passée du design graphique au développement web. Après avoir terminé ma formation en développement web, je suis actuellement à la recherche de nouvelles opportunités pour valoriser mes compétences en tant que développeur web frontend. ",
+        description: "Starting with graphic design, I discovered a deep passion for coding, which led me to transition into the field of web development. After successfully completing a specialized web development training program, I am now seeking new opportunities to showcase my skills and contribute to creative and innovative projects as a frontend web developer.",
         jobTitle: "Frontend Developer",
         secondJob: "Graphic Designer"
     }
@@ -18,77 +17,68 @@ const profilInfo = [
 export default function Profile() {
     const data = profilInfo[0]
     return (
-        <div className="flex flex-wrap items-center justify-center mt-10 p-5 bg-gray-100 rounded-2xl py-10">
+        <div className="flex flex-wrap items-center justify-center mt-10 p-5 bg-backgroundpy-10">
             <div className="lg:w-[30%] md:w-1/3 lg:mb-0 sm:mb-4 sm:w-[90%] flex items-center justify-center">
-                <div className="w-[200px] h-[200px] rounded-full overflow-hidden">
+                <div className="w-[210px] h-[210px] rounded-3xl overflow-hidden hover:border-4 hover:border-primary transform transitio-all rotate-6 duration-300 hover:rotate-0 drop-shadow-xl">
                     <Image
                         src={data.src}
                         alt="profil-yoonju"
-                        className="w-full animation-set-image "
+                        className="w-full scale-105"
                     />
                 </div>
             </div>
             <div className=' lg:w-[70%] md:w-2/3 md:pl-6 pt-10 sm:px-6 lg:pt-1 md:pt-1'>
                 <div className="text-left pl-4 pr-8 flex items-center justify-between">
-                    <h2 className="font-semibold title-font  text-gray-700 text-2xl">
-                        {data.name}
-                    </h2>
+                    <h1 className="font-semibold title-font text-2xl">
+                        I'm {data.name}
+                    </h1>
                     <div className="flex items-center">
-                        <ToolTip
-                            text="Go to my GitHub"
+                        <ButtonLink
+                            name=""
+                            href="#"
+                            size="icon"
+                            variant="ghost"
                         >
-                            <ButtonLink
-                                name=""
-                                href="#"
-                                size="icon"
-                                variant="ghost"
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="2"
+                                stroke="currentColor"
+                                className="size-4 text-foreground"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="2"
-                                    stroke="currentColor"
-                                    className="size-4 text-gray-700"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                                    />
-                                </svg>
-                            </ButtonLink>
-                        </ToolTip>
-                        <ToolTip
-                            text="Download my CV"
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                                />
+                            </svg>
+                        </ButtonLink>
+                        <ButtonLink
+                            name=""
+                            href="#"
+                            size="icon"
+                            variant="ghost"
                         >
-                            <ButtonLink
-                                name=""
-                                href="#"
-                                size="icon"
-                                variant="ghost"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="2"
-                                    stroke="currentColor"
-                                    className="size-4 text-gray-700">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                                    />
-                                </svg>
-                            </ButtonLink>
-                        </ToolTip>
-
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="2"
+                                stroke="currentColor"
+                                className="size-4 text-foreground">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                                />
+                            </svg>
+                        </ButtonLink>
                     </div>
                 </div>
                 <h3 className="text-base font-medium text-primary/80 pl-4">{data.jobTitle} | {data.secondJob}</h3>
                 <div className="sm:pl-4 mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                    <p className="leading-relaxed text-base text-gray-700">
+                    <p className="leading-relaxed text-base">
                         {data.description}
                     </p>
                 </div>
