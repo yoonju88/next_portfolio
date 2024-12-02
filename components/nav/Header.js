a13325b24a8c2211a1d79625a4d514b4cb765409
yoonju88 <yoonju.t@gmail.com>
2024-12-02 15:04:58 +0100

    change text color

--- components/nav/Header.js
+++ components/nav/Header.js
@@ -15,8 +15,8 @@ export default function header() {
     return (
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <Link href='/' className="flex title-font font-medium items-center mb-4 md:mb-0 bg-primary p-1 py-2 rounded-full hover:bg-primary/90" >
-                    <span className="ml-3 text-xl text-white font-semibold" >YT</span>
+                <Link href='/' className="flex title-font font-medium items-center mb-4 md:mb-0" >
+                    <span className="ml-3 text-xl text-primary/80  font-semibold" >TERRENOIRE.Y</span>
                 </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
 
