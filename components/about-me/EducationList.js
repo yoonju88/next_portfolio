d777f1f976a86072bf47dc8454a20486d614a448
yoonju88 <yoonju.t@gmail.com>
2024-12-02 15:46:15 +0100

    restyling with card drop shadow when hover

--- components/about-me/EducationList.js
+++ components/about-me/EducationList.js
@@ -9,25 +9,25 @@ export default function EducationList({ data }) {
                 const startDate = item.duration.start
                 const endDate = item.duration.end
                 return (
-                    <div className=" w-[98%] py-4 px-4 flex items-center justify-center bg-gray-50 rounded-xl drop-shadow-md" key={item.name}>
-                        <div className="w-12 h-12 inline-flex items-center rounded-full bg-white flex-shrink-0 overflow-hidden drop-shadow-md">
+                    <div className=" w-[98%] flex items-center justify-center  card-set-shadow " key={item.name}>
+                        <div className="w-12 h-12 inline-flex items-center bg-white rounded-full flex-shrink-0 overflow-hidden drop-shadow-md shadow-gray-500">
                             <Link href={item.link} target="_blank">
                                 <Image src={item.image} alt="school logo" className="w-full object-cover animation-set-image" />
                             </Link>
                         </div>
                         <div className="flex-grow pl-7">
-                            <p className='text-gray-500 text-xs font-semibold mb-0.5'>
+                            <p className='text-xs font-semibold mb-0.5'>
                                 {startDate} - {endDate}
                             </p>
-                            <h2 className="text-gray-700 text-base title-font font-semibold mb-0.5 flex flex-wrap items-center">
+                            <h2 className=" text-base title-font font-semibold mb-0.5 flex flex-wrap items-center">
                                 {item.name}  &nbsp;
                                 <span className="tracking-widest text-xs font-medium" > ({item.degree})</span>
                             </h2>
-                            <h3 className="tracking-widest text-xs title-font font-medium text-gray-500">
+                            <h3 className="tracking-widest text-xs title-font font-medium ">
                                 <Link href={item.link} target="_blank" className='pr-2 hover:font-semibold'>
                                     {item.schoolName}
                                 </Link>
-                                <span className="border-l-2 pl-2 border-gray-400 text-wrap">
+                                <span className="border-l-2 pl-2 text-wrap">
                                     {item.address}
                                 </span>
                             </h3>
--- components/about-me/ExperienceLists.js
+++ components/about-me/ExperienceLists.js
@@ -8,15 +8,15 @@ export default function ExperienceLists({ data }) {
                 const endDate = item.duration.end
                 return (
 
-                    <div className="h-full flex items-start lg:flex-row md:flex-col sm:flex-col" key={item.jobTitle}>
+                    <div className="h-full flex items-start lg:flex-row md:flex-col sm:flex-col card-set-shadow" key={item.jobTitle}>
                         <div className="w-30 flex-shrink-0 pt-1 flex flex-col text-center leading-none mb-1">
-                            <span className="text-gray-500 border-gray-200 text-xs font-semibold">{startDate} - {endDate}</span>
+                            <span className="border-gray-200 text-xs font-semibold text-foreground/75">{startDate} - {endDate}</span>
                         </div>
                         <div className="flex-grow lg:pl-7 md:pl-0 sm:pl-0">
-                            <h1 className="title-font text-base font-semibold text-gray-700 mb-1">
+                            <h1 className="title-font text-base font-semibold mb-1">
                                 {item.jobTitle}
                             </h1>
-                            <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                            <h2 className="tracking-widest text-xs title-font font-medium mb-1">
                                 <span className='pr-2'>
                                     {item.companyName}
                                 </span>
@@ -24,7 +24,7 @@ export default function ExperienceLists({ data }) {
                                     {item.address}
                                 </span>
                             </h2>
-                            <p className="leading-relaxed mb-2 text-sm text-gray-700">
+                            <p className="leading-relaxed mb-2 text-sm">
                                 {item.description}
                             </p>
                         </div>
