250f2c20005d0556fe2124b02ebbc3367c562b59
yoonju88 <yoonju.t@gmail.com>
2024-12-02 15:05:31 +0100

    correction class to className

--- components/works/ProjectDetailContainer.js
+++ components/works/ProjectDetailContainer.js
@@ -1,6 +1,6 @@
 export default function ProjectDetailContainer({ children }) {
     return (
-        <div class="flex border-t border-gray-200  justify-between py-2 items-center">
+        <div className="flex border-t border-gray-200  justify-between py-2 items-center">
             {children}
         </div>
     )
