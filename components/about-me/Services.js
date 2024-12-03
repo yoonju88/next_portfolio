cb8a6c4c21d9932c0acff82e963c8a6a134a0461
yoonju88 <yoonju.t@gmail.com>
2024-12-03 13:43:42 +0100

    remove descriptions

--- components/about-me/Services.js
+++ components/about-me/Services.js
@@ -33,8 +33,7 @@ export default function Services() {
         <section className="text-gray-600 body-font">
             <div className="container lg:w-[90%] px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-foreground/80">Services</h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-foreground/80">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-foreground/80"> My Services</h1>
                 </div>
                 <ServiceList data={serviceData} />
             </div>
