1b4c984b59a693e7eb2269a692515cb148d2ced7
yoonju88 <yoonju.t@gmail.com>
2024-12-03 13:42:49 +0100

    Change font

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,21 @@
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 import Header from '@/components/nav/Header'
 import Footer from '@/components/Footer'
 import Providers from "./providers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Yoonju Portfolio by NextJs ",
-  description: "yoonju portfolio",
+  description: "With a wide range of services including web development, design, and content creation, I provide creative and efficient solutions. Check out my work and let’s start a project together.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+        className={`${inter.className} antialiased bg-background text-foreground`}
       >
         <Providers >
           <Header />
