a659f30fc1a3065e2f3a5576aa928d52f2972036
yoonju88 <yoonju.t@gmail.com>
2024-12-06 15:01:16 +0100

    Update class value

--- components/works/ProjectTitle.js
+++ components/works/ProjectTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ProjectTypeTitle({ title, description }) {
     return (
-        <div className="flex flex-col items-center gap-10  w-full mb-20">
+        <div className="flex flex-col items-center gap-10  w-full">
             <div className="mb-6 lg:mb-0">
                 <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 capitalize">{title}</h1>
             </div>
