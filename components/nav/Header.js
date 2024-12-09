a16c3d6c27170040b9b70ed27be793f5f4b26736
yoonju88 <yoonju.t@gmail.com>
2024-12-09 12:40:26 +0100

    changed name

--- components/nav/Header.js
+++ components/nav/Header.js
@@ -15,7 +15,7 @@ export default function header() {
         <header className="text-gray-600 body-font">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link href='/' className="flex title-font font-medium items-center mb-4 md:mb-0" >
-                    <span className="ml-3 text-xl text-primary/80  font-semibold" >TERRENOIRE.Y</span>
+                    <span className="ml-3 text-xl text-primary/80  font-semibold" >Yoonju.T</span>
                 </Link>
                 <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
 
