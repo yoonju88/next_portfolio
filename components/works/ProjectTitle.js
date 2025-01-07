c239182bc4c67b6e7e00820ec1924a5639c60f53
yoonju88 <yoonju.t@gmail.com>
2025-01-07 12:04:22 +0100

    Corrected text width

--- components/works/ProjectTitle.js
+++ components/works/ProjectTitle.js
@@ -6,7 +6,7 @@ export default function ProjectTypeTitle({ title, description }) {
             <div className="mb-6 lg:mb-0">
                 <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 uppercase">{title}</h1>
             </div>
-            <p className="lg:w-2/3 w-full leading-relaxed ">{description}</p>
+            <p className="lg:w-2/3 leading-relaxed text-center w-[85%]">{description}</p>
         </div>
     )
 }
