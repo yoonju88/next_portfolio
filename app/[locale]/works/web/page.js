b757104d03fdccd9a75a4aaa8cea41d054158fcd
yoonju88 <yoonju.t@gmail.com>
2025-09-08 16:16:53 +0200

    work - webpage translating

--- app/[locale]/works/web/page.js
+++ app/[locale]/works/web/page.js
@@ -1,22 +1,30 @@
 import React, { Suspense } from 'react'
 import ProjectTitle from '@/components/works/ProjectTitle'
-import { worksData } from '@/utils/worksData'
+import { worksBase } from '@/utils/worksData'
 import WorksCardLoading from '@/components/loading/WorksCardLoading'
 import WorkList from '@/components/works/design/DesignList'
 import { getTranslations } from 'next-intl/server'
 
 export default async function webPage({ params }) {
-    const { locale } = await params
-    const t = await getTranslations({ locale })
+    const { locale } = params;
+    const t = await getTranslations({ locale, namespace: 'web' })
+    const tworks = await getTranslations({ locale, namespace: 'worksweb' })
+
+
+    const items = worksBase.map(w => ({
+        ...w,
+        title: tworks(`${w.id}.title`)
+    }))
+
     return (
         <div className='flex flex-col justify-center mt-20 mb-40'>
             <ProjectTitle
-                title={t("web.title")}
-                description={t("web.description")}
+                title={t("title")}
+                description={t("description")}
             />
             <Suspense fallback={<WorksCardLoading />}>
                 <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-center mt-16 w-full px-10 ">
-                    {worksData.map((item) => {
+                    {items.map((item) => {
                         return (
                             <WorkList
                                 key={item.id}
@@ -24,7 +32,7 @@ export default async function webPage({ params }) {
                                 name={item.title}
                                 cover={item.image}
                                 type={item.type}
-                                link={`/works/web/${item.id}`}
+                                link={`/${locale}/works/web/${item.id}`}
                             />
                         )
                     })}
--- utils/worksData.js
+++ utils/worksData.js
@@ -3,58 +3,50 @@ import Kasa from '@/public/kasa_v2.png'
 import ArgentBank from '@/public/argentbank-v2.png'
 import Event from '@/public/724events-v2.png'
 
-export const worksData = [
+export const worksBase = [
     {
         id: "a15",
         type: "web",
         title: "ArgentBank",
-        tags: ["React", "Nodejs", "Redux", "MongoDB", "API", "Swagger"],
         link: "https://github.com/yoonju88/ArgentBank-website",
         duration: {
             startDate: "2024-07-22",
             endDate: "2024-08-20"
         },
-        image: ArgentBank,
-        description: "I created a responsive dashboard and managed the application’s global state using React and Redux, integrating the back-end through REST API calls. I defined the API endpoints with Swagger and used Node.js to handle the server."
+        image: ArgentBank
     },
     {
         id: "a14",
         type: "web",
         title: "724 events",
-        tags: ["React", "Nodejs", "SEO", "Test unit", "Debug",],
         link: "https://github.com/yoonju88/Projet10-724events",
         duration: {
             startDate: "2024-06-15",
             endDate: "2024-07-20"
         },
         image: Event,
-        description: "I reviewed and fixed bugs using Chrome DevTools and React Developer Tools. I resolved issues with JavaScript and React, completed the test plan with unit and functional tests, and used Yarn and Node.js for dependency management and test execution."
     },
     {
         id: "a13",
         type: "web",
         title: "Kasa",
-        tags: ["React", "Fimga"],
         link: "https://github.com/yoonju88/projet_8_Kasa",
         duration: {
             startDate: "2024-05-08",
             endDate: "2024-06-01"
         },
-        image: Kasa,
-        description: "I developed a responsive web application using React. I set up navigation between different pages of the application with React Router and built the web interface elements using React components."
+        image: Kasa
     },
     {
         id: "a12",
         type: "web",
         title: "Architecture",
-        tags: ["JavaScript", "CSS", "Nodejs", "API", "Fimga"],
         link: "https://github.com/yoonju88/Architecte-sophie-bluel",
         duration: {
             startDate: "2024-04-08",
             endDate: "2024-05-05"
         },
         image: Sophie,
-        description: "I created a dynamic webpage with JavaScript, which includes a login page and a modal for media uploads. I integrated an API for the backend and used Figma to design and implement the layout. Additionally, I utilized Kanban to manage and organize the development process, ensuring efficient task tracking and collaboration."
     },
 ]
 
