9e40b5b8f03673b675ac4f55933ccb68eced4ea2
yoonju88 <yoonju.t@gmail.com>
2025-09-16 17:17:21 +0200

    Resize loading title

--- app/[locale]/Loading.js
+++ app/[locale]/Loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="fixed inset-0 z-[9999] flex items-center justify-center" >
-            <div className="loader" />
+            <div className="loader flex items-center justify-center" />
         </div >
     );
 }
