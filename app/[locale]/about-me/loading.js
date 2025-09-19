35166cffd45ea893e64346b385ad6f0209c3bba2
yoonju88 <yoonju.t@gmail.com>
2025-09-19 14:14:53 +0200

    updated loadiing style

--- app/[locale]/about-me/loading.js
+++ app/[locale]/about-me/loading.js
@@ -6,15 +6,15 @@ export default function loading() {
     return (
         <div className='animate-pulse gap-10 flex flex-wrap justify-center items-center px-10'>
             <div className="w-[30%]">
-                <Skeleton className="h-[300px] w-full" />
+                <Skeleton className="h-[300px] w-full bg-background/20" />
             </div>
             <div className="w-[65%] flex flex-col gap-4 pl-6">
-                <Skeleton className="h-[50px] w-full mb-4" />
-                <Skeleton className="h-[30px] w-full" />
-                <Skeleton className="h-[30px] w-full" />
-                <Skeleton className="h-[30px] w-full" />
-                <Skeleton className="h-[30px] w-full" />
-                <Skeleton className="h-[30px] w-full" />
+                <Skeleton className="h-[50px] w-full mb-4 bg-background/20" />
+                <Skeleton className="h-[30px] w-full bg-background/20" />
+                <Skeleton className="h-[30px] w-full bg-background/20" />
+                <Skeleton className="h-[30px] w-full bg-background/20" />
+                <Skeleton className="h-[30px] w-full bg-background/20" />
+                <Skeleton className="h-[30px] w-full bg-background/20" />
             </div>
         </div>
     )
