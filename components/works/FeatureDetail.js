19d0b9cbd1d3bcef6db42ad80518407d3542f10e
yoonju88 <yoonju.t@gmail.com>
2025-10-02 15:21:57 +0200

    update subtitle color

--- components/works/FeatureDetail.js
+++ components/works/FeatureDetail.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FeatureDetail({ title, data }) {
     return (
         <div className=" p-6">
-            <h4 className="text-btn-1 font-bold mb-3">{title}</h4>
+            <h4 className="text-color_subtitle font-bold mb-3">{title}</h4>
             <ul className="list-disc list-inside space-y-2 text-foreground/90">
                 {data.map((sentence, idx) => (
                     <li key={idx}>{sentence}</li>
