a9699822c778359f9161dc25b1f608322caaa97e
yoonju88 <yoonju.t@gmail.com>
2025-10-02 14:30:25 +0200

    add subtitle

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -21,15 +21,17 @@ export default function Home() {
   }
   return (
     <div className="sm:px-20 px-4 flex flex-col justify-center items-center text-center">
-      <h1 className="relatitle-font lg:text-7xl  flex flex-wrap md:text-5xl text-[28px] mb-10 font-extrabold sm:uppercase 
+
+      <h1 className="relatitle-font lg:text-7xl  flex flex-wrap md:text-5xl text-[28px] mb-6 font-extrabold sm:uppercase 
         overflow-hidden whitespace-nowrap tracking-wide
         text-transparent stroke-title-intro
         animate-typing"
       >
         {parse(t('title'))}
       </h1>
+      <h2 className="text-color_subtitle mb-2 lg:text-3xl md:text-xl text-lg animate-slide-down-soft [animation-delay:2200ms]">{t('subtitle')}</h2>
       <p
-        className="sm:mb-32 mb-16 lg:text-2xl md:text-lg leading-relaxed text-foreground/80 sm:w-[80%] w-[90%] animate-slide-down-soft  [animation-delay:2200ms]"
+        className="sm:mb-32 mb-16 lg:text-2xl md:text-lg leading-relaxed text-foreground/80 sm:w-[80%] w-[90%] animate-slide-down-soft  [animation-delay:2600ms]"
       >
         {t('description')}</p>
       <ArrowBtn name={t("button3")} link={`${locale}/works/web`} animation="animate-slide-down-soft  [animation-delay:3000ms]" />
